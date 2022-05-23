e5ca3bbbb9bdb4e12b1b40fcfe279bb458d1b9d8
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-05-23 13:52:41 +0200

    some aclarations about reference

--- assets/js/valor-referencia.js
+++ assets/js/valor-referencia.js
@@ -1,22 +1,22 @@
-/*PASO POR VALOR -> Cualquier tipo de asignación
-**o paso de parametro a función es UN VALOR, NO UNA REFERENCIA
-**A UNA REGIÓN DE LA MEMORIA.
+/*
+PASO POR VALOR -> Cualquier tipo de asignación o paso de parametro a función es UN VALOR, NO UNA REFERENCIA A UNA REGIÓN DE LA MEMORIA.
 */
-let a = 10;
-let b = a;
-a = 30;
-console.log({a, b});
+// let a = 10; //por valor
+// let b = a;  //por valor (el valor de a)
+// a = 30;
+// console.log({a, b});
 
 /*PASO POR REFERENCIA -> Todos los Objetos se pasan por Referencia*/
-let juan = { nombre: 'Juan'};
-// let ana  = juan;
-// let ana  = {juan};
-let ana  = {...juan}; //...operador Spread , sería como decir clonar el objeto juan
+let juan = { nombre: 'Juan' };
+// let ana  = juan; //ana es juan, si le cambio el nombre a ana, se lo cambio a juan
+// let ana  = {juan}; //ana contiene un objeto juan
+let ana  = {...juan}; //...operador Spread , ana es un Clon de juan, pero no es juan
+
 ana.nombre = 'Ana';
 console.table({juan, ana});
 
 //CON ARRAYS
-const frutas = ['Manzana', 'Pera', 'Piña'];
-const otrasFrutas = [...frutas, 'Mango'];
+// const frutas = ['Manzana', 'Pera', 'Piña'];
+// const otrasFrutas = [...frutas, 'Mango'];
 // otrasFrutas.push('Mango');
-console.table({frutas, otrasFrutas});
+// console.table({frutas, otrasFrutas});
